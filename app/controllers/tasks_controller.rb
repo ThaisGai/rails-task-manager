7c25ff3fe6d9d12e58a179c52907deef9677a3df
Thais Almeida Gai <thais.gabriel@gmail.com>
2026-02-15 19:43:52 -0300

    Exercicio Resolvido

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,30 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:restaurant])
+    @task = Task.new(task_params)
     @task.save
+    redirect_to task_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.new(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.new(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root "tasks#index"
   get "tasks/new", to: "tasks#new"
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  get "tasks/:id", to: "tasks#show", as: "task"
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
